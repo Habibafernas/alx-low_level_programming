384830754640662361e6bd332b7a7057e2473047
HabibafernaS <habibafernas@gmai.com>
2023-06-15 13:53:22 -0700

    this is my first try

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  * */
 int main(void)
 {
-	printf(" short int is %2d bytes \n", sizeof(short int));
-        printf(" int is %2d bytes \n", sizeof(int));
-	printf(" int * is %2d bytes \n", sizeof(int *));
-	printf(" long int is %2d bytes \n", sizeof(long int));
-	printf(" long int * is %2d bytes \n", sizeof(long int *));
+	printf(" Size of a char: %2d byte(s) \n", sizeof(char));
+        printf(" Size of a int: %2d byte(s) \n", sizeof(int));
+	printf(" Size of a long int: %2d bytes \n", sizeof(long int));
+	printf(" Size of a long long int: %2d byte(s) \n", sizeof(long int *));
+	printf(" Size of a float: %2d byte(s) \n", sizeof(float));
 	return (0);
 }
