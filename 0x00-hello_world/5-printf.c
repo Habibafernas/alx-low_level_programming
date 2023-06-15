0d554b8bed2f1c69cf059b0e1fc85c3295b0663a
HabibafernaS <habibafernas@gmai.com>
2023-06-15 13:30:58 -0700

    this is my first try

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* 
+ * this is main function to print "with proper grammar, but the outcome is a piece of art,"
+ * and return (0)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
