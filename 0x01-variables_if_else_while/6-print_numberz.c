d0cde911718808caed13e136103f80d8006f4e44
HabibafernaS <habibafernas@gmai.com>
2023-06-16 15:02:32 -0700

    this is my first try

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+/**
+ * main - Entry point
+ *
+ * Description : prints all numbers from 0 to 9
+ *
+ * Return: Always 0 (success)
+ */
+#include<stdio.h>
+int main(void)
+{
+	int n;
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+	putchar('n');
+	return (0);
+}
