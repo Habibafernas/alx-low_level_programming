77c30879508a9ee41d36c488a1b1364c7cba8dd2
HabibafernaS <habibafernas@gmai.com>
2023-06-16 14:35:17 -0700

    this is my first try

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+/**
+ * main - Entry point
+ *
+ * Description : prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
+#include<stdio.h>
+int main (void)
+{
+	char n;
+	for(n = 'a'; a <= 'z' && n != 'q' && n != 'e'; a++)
+	{
+		putchar(n);
+
+	}
+	putchar('\n');
+	return (0);
+}
