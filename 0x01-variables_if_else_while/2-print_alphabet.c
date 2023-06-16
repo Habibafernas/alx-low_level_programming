e8db7611be301854bd37374c69ebd81cb0aec79c
HabibafernaS <habibafernas@gmai.com>
2023-06-16 13:39:34 -0700

    this is my first commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+/**
+ * main - Entry point
+ *
+ * Description : prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (success)
+ */
+#include<stdio.h>
+int main(void)
+{
+char a;
+	for (a='a', a < 'z' , a++)
+	{
+		putchar(a);
+	}
+return (0);
+}
