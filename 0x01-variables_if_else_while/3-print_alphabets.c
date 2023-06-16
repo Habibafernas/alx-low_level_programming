44cbb29b17a373f556dac1a709d01b99825c76d0
HabibafernaS <habibafernas@gmai.com>
2023-06-16 13:58:18 -0700

    this is my first commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+/**
+ * main - Entry point
+ *
+ * Description : prints all the alphabets in lowercase then in uppercase
+ *
+ * Return: Always 0 (success)
+ */
+#include<stdio.h>
+int main(void)
+{
+	char n, N;
+	for (n='a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	for (N='A'; N <= 'Z'; N++)
+	{
+		putchar(N);
+	}
+	putchar('\n')
+}
