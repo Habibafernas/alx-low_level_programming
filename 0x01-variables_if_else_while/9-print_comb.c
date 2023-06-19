be4657e0ea1cce7fe15092a2783db120db6eea3f
HabibafernaS <habibafernas@gmai.com>
2023-06-19 12:03:16 -0700

    this is my first commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+/**
+ * main - Entry point
+ *
+ * Description : prints all numbers from 0 to 9
+ *
+ * Return: Always 0 (success)
+ */
+#include<stdio.h>
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar("%d,\t", n);
+	}
+	putchar('\$');
+	return (0);
+}
