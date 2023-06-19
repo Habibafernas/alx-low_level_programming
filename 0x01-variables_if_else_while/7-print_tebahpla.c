d57ce46603bb427168b19bd5e2372c7392e81da4
HabibafernaS <habibafernas@gmai.com>
2023-06-19 11:48:21 -0700

    this is my first commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+/**
+ * main - Entry point
+ *
+ * Description : rints the lowercase alphabet in reverse
+ *
+ * Return: Always 0 (success)
+ */
+#include<stdio.h>
+int main(void)
+{
+	char n;
+
+	for(n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+}
