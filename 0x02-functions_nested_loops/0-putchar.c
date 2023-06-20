689bf3c92ac889db1206cffdf41e4fea71c114e3
HabibafernaS <habibafernas@gmai.com>
2023-06-20 14:26:46 -0700

    this is first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+/**
+ * main - Entry point
+ *
+ * Description : prints all numbers from 0 to 9
+ *
+ * Return: Always 0 (success)
+ */
+#include<stdio.h>
+int main(void)
+{
+        putchar("_putchar");
+        return (0);
+}
