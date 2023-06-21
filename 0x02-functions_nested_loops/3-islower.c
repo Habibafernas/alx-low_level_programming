c80cc7a27e2041fefa7c3cd53e21b8105055613b
HabibafernaS <habibafernas@gmai.com>
2023-06-21 07:56:54 -0700

    this is my first commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 /**
  * _islower - check the code
  *
- * Description : checking if 'c' is lowercase or not
+ * Description : checking if 'c' is lowercase or not in ASCII code
  *
- * Return : 1 if 'c' is lowercase and it reurns 0 otherwise
+ * Return : 1 if 'c' is lowercase and 0 otherwise
  */
 int _islower(int c)
 {
