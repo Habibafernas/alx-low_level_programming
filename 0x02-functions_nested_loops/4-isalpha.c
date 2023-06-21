57b311a702aef8d10d93355a2e629b62211497a4
HabibafernaS <habibafernas@gmai.com>
2023-06-21 08:41:25 -0700

    this is my first commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,12 +9,15 @@
  */
 int _isalpha(int c)
 {
-	if (132 >= c >= 41 && 61 >= c >= 172)
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
 	else
 	{
-		return (0);
-	}
+		return (0);}
 }
