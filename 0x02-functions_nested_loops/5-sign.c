4e892932b709f4b024e7cc3ff3a723f505f2014b
HabibafernaS <habibafernas@gmai.com>
2023-06-21 09:03:24 -0700

    this is my first commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+/**
+ * print_sign - check the code
+ *
+ * Description : checking if 'n' is negative,positive or zero
+ *
+ * @n variable : n variable is integer
+ *
+ * Return: 1 if 'c' is lowercase and 0 otherwise
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else 
+	{
+		_putchar('0');
+		return (0);
+	}
+}
