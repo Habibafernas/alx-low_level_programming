5cc485b60c7559cd092f93ad6ea565d7bf14622a
HabibafernaS <habibafernas@gmai.com>
2023-06-21 09:34:15 -0700

    this is my first commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _abs - check the code
+ *
+ * Description : getting the abs value of n
+ *
+ * @c variable : c variable is integer
+ *
+ * Return: abs value of n
+ */
+int _abs(int)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else 
+	{
+		return(n);
+	}
+}
+
