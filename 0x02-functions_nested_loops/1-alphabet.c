a258425b9c2a93e10eb200646668f2718df2ec70
HabibafernaS <habibafernas@gmai.com>
2023-06-21 05:11:30 -0700

    this is my first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
+void print_alphabet(void)
+{
+	char a;
+
+	for(a = 'a'; a <= z; a++)
+	{
+		_putchar(a);
+
+	}	
+	_putchar('\n');
+}
