51d36326b413f59c7e55a4a9d6c198cae6c4e564
HabibafernaS <habibafernas@gmai.com>
2023-06-21 06:45:40 -0700

    this is my first commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
-/**
+/*
  * _islower - Entry Point
  *
  * Description : function checks for lowercase character of 'c' values
  *
- * Return : Return 1 if 'c' is lowercase Return 0 otherwise.
+ * Return : if 'c' is lowercase it returns 1 otherwise it reurns 0.
  */
 int _islower(int c)
 {
