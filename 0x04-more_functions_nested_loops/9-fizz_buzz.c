dd274cf37e88c34368b37a044339b1ef48a4c739
HabibafernaS <habibafernas@gmai.com>
2023-06-22 13:28:51 -0700

    this is my first commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - check the code
+ *
+ * Description : checking if 'c' is lowercase or uppercase
+ *
+ * @c variable : c variable is values fron ASCII Code
+ *
+ * Return: 1 if 'c' is lowercase and 0 otherwise
+ */
+int Fizz-Buzz(int n)
+{
+	for (n; n == )
+}
