669ca541ce8da6f04a167deb22d08253523e522d
HabibafernaS <habibafernas@gmai.com>
2023-06-22 12:59:34 -0700

    this is my first commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,11 +5,12 @@
  * Description : function that draws a straight line in the terminal
  *
  * @n : n is an integer
+ *
  * Return: Always (0)
  */
 void print_line(int n)
 {
-	while (n != 0)
+	while(n != 0)
 	{
 		_putchar('_');
 		n--
