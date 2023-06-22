1cca72f40f6b1ff58062a0ccf719b2fe05c202b9
HabibafernaS <habibafernas@gmai.com>
2023-06-22 12:19:27 -0700

    this is my first commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - check the code
+ *
+ * Description : function that prints the numbers, from 0 to 9
+ *
+ * Return: Always (0)
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(a);
+	}
+	_putchar("\n");
+	return (0);
+}
