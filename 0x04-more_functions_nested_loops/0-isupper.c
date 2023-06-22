fa4ada1a58abb96c3763f8b7d380f89b2f2e416b
HabibafernaS <habibafernas@gmai.com>
2023-06-22 11:37:03 -0700

    this is my first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+/**
+ * _islower - check the code
+ *
+ * Description : checking if 'c' is lowercase or uppercase
+ *
+ * @c variable : c variable is values fron ASCII Code
+ *
+ * Return: 0 if 'c' is lowercase and 1 otherwise
+ */
+int _isupper(int c);
+{
+	if (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else if (c >= 97 && c <= 122)
+	{
+		return (0);
+	}
+}
