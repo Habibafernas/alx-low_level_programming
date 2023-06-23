517830a50d414a4e4d161a182734683ee15da695
HabibafernaS <habibafernas@gmai.com>
2023-06-23 09:53:26 -0700

    this is my first commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,14 +10,20 @@
  */
 void print_diagonal(int n)
 {
+	int i, j;
+
 	if (n <= 0)
 	{
 		_putchar('\n')
 	}
-	while (n != 0 && n > 0)
+	for (i = 0; i < n; i++)
 	{
+		for (int j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
 		_putchar('\\');
-		n--
+		_putchar('\n');
 	}
 	_putchar('\n');
 }
