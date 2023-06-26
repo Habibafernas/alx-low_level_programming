a2942b6923b15f7770807a2273c7a08956ddf0f9
HabibafernaS <habibafernas@gmai.com>
2023-06-26 11:41:50 -0700

    this is my commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,16 @@
 /**
- * main - Entry point
+ * print_rev - Entry point
  *
- * Description : prints all numbers from 0 to 9
+ * Description : Write a function that prints a string, in reverse
+ *
+ * @s : char variable
  *
  * Return: Always 0 (success)
  */
 void print_rev(char *s)
 {
 	int i;
+
 	for (i = 0; s[i] != 0; i++)
 	{
 		_putchar(s[((sizeof(s) - 1) - i)]);
