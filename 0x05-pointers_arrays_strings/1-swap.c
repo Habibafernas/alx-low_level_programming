2ca45ee823f91fd6cd6a7e093b4e353976da73dc
HabibafernaS <habibafernas@gmai.com>
2023-06-26 04:13:41 -0700

    this is my first commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - determine value of pointer
+ * @a: integer variable
+ * @b: integer variable
+ */
+void swap_int(int *a, int *b)
+{
+	int n;
+
+	n = *a;
+	*b = n;
+	*a = *b;
+}
