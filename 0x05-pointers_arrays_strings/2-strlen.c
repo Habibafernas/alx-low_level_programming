dcb46c392f4841aee3fc429890bf889cef392c16
HabibafernaS <habibafernas@gmai.com>
2023-06-26 04:22:03 -0700

    this is my first commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - determine length of a pointer
+ * Description : determine length of a pointer
+ * @s: integer variable
+ * Return: return size of array
+ */
+int _strlen(char *s)
+{
+	int l;
+
+	l = sizeof(*s);
+}
