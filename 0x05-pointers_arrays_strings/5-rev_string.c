ec74306e1000c35305aedd29b8e062e25b48144a
HabibafernaS <habibafernas@gmai.com>
2023-06-26 11:58:01 -0700

    this is my commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * rev_string - draws a diagonal line on the terminal
+ * @s: number of times the character \ should be printed
+ */
+void rev_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		_putchar(s[((strlen(s) - 1) - i)]);
+	}
+	_putchar('\n');
+}
