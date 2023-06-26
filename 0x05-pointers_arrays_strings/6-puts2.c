9f8e468789a4d22105ada4a6854d12dfadf764d8
HabibafernaS <habibafernas@gmai.com>
2023-06-26 14:18:09 -0700

    this is my commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - determine value of pointer
+ * @str: char variable
+ */
+void puts2(char *str)
+{
+	char *ch = "";
+
+	int i, j;
+
+	for (i = 0; i != '\0' && i % 2 == 0; i++)
+	{
+		for (j = 0; j != '\0'; j++)
+		{
+			ch[j] = str[i];
+			j--;
+			break;
+		}
+	}
+	str = ch;
+}
