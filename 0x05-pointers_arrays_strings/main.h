7ba817f18869ef7b957f9ab3f45f1cd1ef3960f9
HabibafernaS <habibafernas@gmai.com>
2023-06-26 07:28:00 -0700

    this is my commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,8 @@
 #define FILE_MAIN
 #include <stdio.h>
 #include<string.h>
+void _putchar(char c);
+void _puts(char *str);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
