55982f604ea596e4d2a4caaa47aab8a573714341
HabibafernaS <habibafernas@gmai.com>
2023-06-26 15:55:38 -0700

    this is my commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the times table
+ * print_array - prints the times table
  * @n: integer for which the times table will be printed
  *
+ * @a : char variable
  * Description: prints the times table
  *
  * Return: void
