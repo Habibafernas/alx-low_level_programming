a8bdb72cb3be4f960b1246b4e4b9d823493fff4e
HabibafernaS <habibafernas@gmai.com>
2023-06-29 13:20:10 -0700

    this is my first commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,21 +2,22 @@
 
 /**
  ** _strncat - a function that concatenates two strings
- * @str: integer variable
+ * @src: integer variable
  * @dest: int variable
- * n: integer variable
+ * @n: integer variable
  * Description: prints the times table
+ * Return: returns dest which is concatenation of dest anf src
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i , j;
+	int i, j;
 
-	i=0;
-	j=0;
+	i = 0;
+	j = 0;
 
-	while(dest[i] != '\0')
-		i++
-	while(src != '\0' && j < n)
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
 		i++;
