8a5089bfaf77310502da149e217ff7aed6b891d8
HabibafernaS <habibafernas@gmai.com>
2023-06-29 09:56:25 -0700

    this is my first commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * *_strcat - prints the times table
+ * @dest: char variable
+ * @src: char rvariable
+ *
+ * Description: function that concatenates two strings
+ */
+char *_strcat(char *dest, char *src)
+{
+	dest = dest + src
+		return (dest);
+}
