13e87272aace8d6faa919fcd2455be2a9749e4a7
HabibafernaS <habibafernas@gmai.com>
2023-06-29 14:29:41 -0700

    this is my first commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,6 +9,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	dest = dest + src
-		return (dest);
+	int i, j;
+	
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
