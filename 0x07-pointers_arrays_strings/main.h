4bab456d9e54cbb110ad50dbc3d7bbc8adc3c39b
HabibafernaS <habibafernas@gmai.com>
2023-07-03 13:52:45 -0700

    this is my commit

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+#include <stdio.h>
+#include<string.h>
+void _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+void _puts(char *str);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *leet(char *);
+#endif
