ff5d9a19af2085f4f60924b2112d4969be31f288
HabibafernaS <habibafernas@gmai.com>
2023-07-07 02:49:31 -0700

    this is my first commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string, followed by a new line.
+ * @s: integer for which the times table will be printed
+ *
+ * Description: prints the times table
+ */
+void _puts_recursion(char *s)
+{
+	if (*S == '\0')
+	{
+		_putchar('\n');
+	}
+	_putchar(*s);
+	_puts_recursion(s+1);
+
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,17 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+#include <stdio.h>
+#include<string.h>
+void _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+void _puts(char *str);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *leet(char *);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+void _puts_recursion(char *s);
+#endif
