9bb504e90adf48a94f6575d443a1147cccf42bc3
HabibafernaS <habibafernas@gmai.com>
2023-07-10 14:45:43 -0700

    this is my first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arrguments
+ *
+ * Description: prints the times table
+ */
+int main(int argc_attribute_((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return(0);
+}
