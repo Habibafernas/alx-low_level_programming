afc834fd365fc8880c91f62b2b1a1135a33bbeec
HabibafernaS <habibafernas@gmai.com>
2023-07-12 08:51:39 -0700

    this is my first commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - a program that prints the number of arguments passed into it.
+ * @argc: number of arguments
+ * @argv: array of arguments
+ */
+int main (int arg, char *argv[]  __attribute__((unused)))
+{
+	printf ("%d\n", argc - 1);
+	return (0);
+}
