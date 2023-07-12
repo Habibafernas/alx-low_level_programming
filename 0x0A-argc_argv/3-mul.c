b47b389bf636c0e281819373c14a32865bb488d8
HabibafernaS <habibafernas@gmai.com>
2023-07-12 11:40:37 -0700

    this is my first commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main -  print the result of the multiplication, followed by a new line
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: return multiplication of two numbers
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	result = argv[1] * argv[2];
+	return (result);
+}
