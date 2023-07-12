fc6175dfb5a49597e7c38fa88a4b96edc24b1047
HabibafernaS <habibafernas@gmai.com>
2023-07-12 08:53:23 -0700

    this is my first commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,7 +4,7 @@
  * @argc: number of arguments
  * @argv: array of arguments
  */
-int main (int arg, char *argv[]  __attribute__((unused)))
+int main(int argc, char *argv[]  __attribute__((unused)))
 {
 	printf ("%d\n", argc - 1);
 	return (0);
