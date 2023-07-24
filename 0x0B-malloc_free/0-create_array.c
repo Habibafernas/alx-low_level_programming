94a138896159f9ecd630d3ad547d5a2ba7627665
HabibafernaS <habibafernas@gmai.com>
2023-07-24 03:58:56 -0700

    this is my first commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * *create_array - a program that prints all arguments it receives.
+ * @size: size of the array
+ * @c: char to initialize the array
+ * Return: pointer to the array (Success), NULL (Error)
+ */
+char *create_array(unsigned int size, char c)
+{
+	int *p , i = 0;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	p = (char *) malloc(sizeof(char) * size);
+	if (p == NULL)
+	{
+		return (0);
+	}
+	while (i < size)
+	{
+		*(p + i) = c;
+		i++;
+	}
+	*(p + i) = '\0';
+	return (p);
+
+}
