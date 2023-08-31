517a3785363acece92c708cf95e9a147e06a742d
habibafernas <you@example.com>
2023-08-31 10:13:14 -0700

    this is my first commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _pow_recursion - a program that prints all arguments it receives.
+ * @x: size of the array
+ * @y: char to initialize the array
+ * Return: pointer to the array (Success), NULL (Error)
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (x ** y);
+	}
+}
