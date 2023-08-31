ef52b887fbe8c9173d3b460bd68116b356d2f0f0
habibafernas <you@example.com>
2023-08-31 13:56:28 -0700

    this is my first commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,12 +7,12 @@
  */
 int is_prime_number(int n)
 {
-	if(n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0)
+	if(n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0 || n % 1 == 0)
 	{
 		return (0);
 	}
 	else
 	{
-	return (1);
+		return (1);
 	}
 }
