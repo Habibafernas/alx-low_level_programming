42634565e9cc04226a3f4eec1cb511d25fced57c
habibafernas <you@example.com>
2023-08-31 13:54:50 -0700

    this is my first commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * is_prime_number - a program that prints all arguments it receives.
+ * @n: size of the array
+ * Return: pointer to the array (Success), NULL (Error)
+ */
+int is_prime_number(int n)
+{
+	if(n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0)
+	{
+		return (0);
+	}
+	else
+	{
+	return (1);
+	}
+}
