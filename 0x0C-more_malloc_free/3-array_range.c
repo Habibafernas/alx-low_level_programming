6033f23cbcdd6bb50928d7c5d37f7c496f7ca416
habibafernas <you@example.com>
2023-09-07 14:59:32 -0700

    i

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,20 +10,25 @@
  */
 int *array_range(int min, int max)
 {
-	char *p, i;
+	int *p;
+	int i;
 
 
 	if (min > max)
 	{
 		return (NULL);
 	}
+
 	p = malloc(sizeof(int) * (max - min + 1));
+
 	if (p == NULL)
 	{
 		return (NULL);
 	}
+
 	for (i = 0; min <= max; i++)
 	{
 		p[i] = min++;
 	}
+	return (p);
 }
