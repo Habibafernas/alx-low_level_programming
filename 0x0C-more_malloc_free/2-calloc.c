3790aa6b1d1e452789c5bd11113ba0577fd41db9
habibafernas <you@example.com>
2023-09-07 14:31:39 -0700

    i

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_calloc - concatenates n bytes of a string to another string
+ * @nmemb: string to append to
+ * @size: string to concatenate from
+ *
+ * Return: pointer to the resulting string
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p, i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(nmemb * size);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < (size * nmemb); i++)
+	{
+		p[i] = 0;
+	}
+	return (p);
+}
