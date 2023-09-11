51b07785b5daba4524300affb7b12dd107c8332d
habibafernas <you@example.com>
2023-09-11 10:58:32 -0700

    this is my first commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,11 @@
 #include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description : prints if the n is positive , negative or zero
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
