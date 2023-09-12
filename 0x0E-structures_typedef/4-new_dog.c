23bf105fd620dd45fc77a3db0ea61d45e00fd66d
habibafernas <you@example.com>
2023-09-12 15:05:34 -0700

    a

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,3 +1,22 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	for(s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
 /**
  * *_strcpy - copies the string pointed to by src
  * including the terminating null byte (\0)
@@ -33,11 +52,33 @@ char *_strcpy(char *dest, char *src)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *dog;
+	int len1, len2;
+
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+
 	if (dog == NULL)
 	{
 		return (NULL);
 	}
+	dog->name = malloc(sizeof(char) * (len1 + 1);
+	if (dog->name = NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (len2 + 1);
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
 	_strcpy(dog->name, name);
 	_strcpy(dog->owner, owner);
-	dog->age = age;	
+	dog->age = age;
+	return (dog);
 }
