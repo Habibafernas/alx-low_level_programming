24593df0b3e8569ec30dec645123f535b6224a18
habibafernas <you@example.com>
2023-09-14 06:15:25 -0700

    o

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		result = va_arg(list, int);
-		printf("%d%s", result, separator);
+		printf("%s%d", separator, result);
 	}
 	va_end(list);
 	printf("\n");
