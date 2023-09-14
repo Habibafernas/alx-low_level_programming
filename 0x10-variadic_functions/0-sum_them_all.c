b27c898e02e32082b318ff314866cab41c43aa18
habibafernas <you@example.com>
2023-09-14 04:24:28 -0700

    o

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,18 +8,21 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int i, sum;
+	int i, sum = 0;
 
 	if (n == 0)
 	{
 		return (0);
 	}
 	va_list list;
+
 	va_start(list, n);
+
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(list, int);
 	}
 	va_end(list);
+
 	return (sum);
 }
