c47e5b8a828d6bcb4d70a540a157cccc38a8bb8e
habibafernas <you@example.com>
2023-09-14 07:03:17 -0700

    o

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", va_arg(list, char *));
 		}
-		if (!va_arg(list, char *))
+		if (!(va_arg(list, char *)))
 		{
 			printf("nil");
 		}
