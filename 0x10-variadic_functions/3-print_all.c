91e71abac00540030aab817b2663f9c66d4999a2
Habibafernas <habibafernas@gmail.com>
2023-09-15 03:31:22 -0700

    a

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,7 +8,7 @@ void print_all(const char * const format, ...)
 {
 	int i;
 
-	char* sep, str;
+	char *sep, *str;
 
 	va_list list;
 
@@ -16,23 +16,23 @@ void print_all(const char * const format, ...)
 
 	if (format)
 	{
-		switch(format[i])
+		switch (format[i])
 		{
 			case 'c':
 				printf("%s%c", sep, va_arg(list, int));
 				break;
 			case 'i':
 				printf("%s%d", sep,  va_arg(list, int));
-                                break;
+				break;
 			case 'f':
-                                printf("%s%f", sep,  va_arg(list, double));
-                                break;
+				printf("%s%f", sep,  va_arg(list, double));
+				break;
 			case 's':
 				str = va_arg(list, char*);
 				if (!str)
 					str = "nil";
-                                printf("%s%s", sep, str);
-                                break;
+				printf("%s%s", sep, str);
+				break;
 			default:
 				i++;
 				continue;
