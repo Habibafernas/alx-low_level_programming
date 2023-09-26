60bcede58abab65c6dfc4da87513b8b083262a2c
Habibafernas <habibafernas@gmail.com>
2023-09-26 13:11:07 -0700

    a

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,6 +10,7 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
+
 	listint_t *t = *head;
 
 	new = malloc(sizeof(listint_t));
@@ -33,4 +34,3 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	t->next = new;
 	return (new);
 }
-
