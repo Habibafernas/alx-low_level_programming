847320e88aa7a404de32b28116730c0fce0c17c1
Habibafernas <habibafernas@gmail.com>
2023-10-02 14:40:05 -0700

    a

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		}
 	}
 	p = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, l);
+	w = write(p, text_content, l);
 	if (p == -1 || w == -1)
 		return (-1);
 	close(p);
