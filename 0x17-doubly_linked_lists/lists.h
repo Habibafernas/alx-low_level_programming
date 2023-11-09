10b161f3fbfbc9d5b95284123bfc5dbad8375ffb
Habibafernas <habibafernas@gmail.com>
2023-11-09 06:20:55 -0800

    a

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -3,5 +3,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
 size_t print_dlistint(const dlistint_t *h);
 #endif
