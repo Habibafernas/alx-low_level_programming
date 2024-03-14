5d1496ec3b05a1cbd4518d1261bef9fe3ac36f3c
Habibafernas <habibafernas@gmail.com>
2024-03-13 19:04:54 -0700

    A

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,15 +4,5 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/**
- * struct listint_s - singly linked list
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
 int linear_search(int *array, size_t size, int value);
 #endif
